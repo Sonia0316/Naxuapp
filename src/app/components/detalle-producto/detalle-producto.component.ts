3494664e76350627331e9877c0c0a78ff0fbc119
Carlos Eduardo Garcia Tejeda <carlos@numina.mx>
2020-07-12 21:58:42 -0500

    Avances

--- src/app/components/detalle-producto/detalle-producto.component.ts
+++ src/app/components/detalle-producto/detalle-producto.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { Routes, Router } from 'node_modules/@angular/router';
+import { ActivatedRoute } from 'node_modules/@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-detalle-producto',
-  templateUrl: './detalle-producto.component.html'
+  templateUrl: './detalle-producto.component.html',
 })
 export class DetalleProductoComponent implements OnInit {
+  public loading = false;
+  public status: string;
+  public dataProduct: any;
 
-  //import { Routes, Router } from 'node_modules/@angular/router';
-  constructor() { }
-  ngOnInit(): void {
-    
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly httpClient: HttpClient
+  ) {}
+  public async ngOnInit(): Promise<void> {
+    this.loading = true;
+    try {
+      const idProducto = Number(this.route.snapshot.paramMap.get('idProducto'));
+      this.dataProduct = ((await this.httpClient
+        .get(
+          `https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/productos/${idProducto}`
+        )
+        .toPromise()) as any).response.lista;
+      console.log('====================================');
+      console.log(this.dataProduct);
+      console.log('====================================');
+
+      this.status = 'complete';
+    } catch (error) {
+      console.log(error);
+      this.status = 'error';
+    } finally {
+      this.loading = false;
+    }
   }
-
 }
--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { Routes, Router } from 'node_modules/@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-productos',
-  templateUrl: './productos.component.html'
+  templateUrl: './productos.component.html',
 })
 export class ProductosComponent implements OnInit {
+  public loading = false;
+  public status: string;
+  public productos = [];
+  public categorias = [];
 
-  //import { Routes, Router } from 'node_modules/@angular/router';
-  constructor() { }
-    ngOnInit(): void {
-    
+  constructor(private readonly httpClient: HttpClient) {}
+  public async ngOnInit(): Promise<void> {
+    this.loading = true;
+    try {
+      // this.categorias: [] = ((await this.httpClient
+      //   .get(
+      //     'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/productos/categorias'
+      //   )
+      //   .toPromise()) as any).response.lista;
+      this.productos = ((await this.httpClient
+        .get(
+          'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/productos'
+        )
+        .toPromise()) as any).response.lista;
+      if (this.productos.length) {
+        this.status = 'complete';
+      } else {
+        this.status = 'empty';
+      }
+    } catch (error) {
+      console.log(error);
+      this.status = 'error';
+    } finally {
+      this.loading = false;
+    }
   }
-
 }
