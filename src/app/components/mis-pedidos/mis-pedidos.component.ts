78659bf34d053330fecc3e8c16d5a5cef505f96c
Carlos Eduardo Garcia Tejeda <carlos@numina.mx>
2020-07-14 19:35:43 -0500

    Pedidos concluido

--- src/app/components/mis-pedidos/mis-pedidos.component.ts
+++ src/app/components/mis-pedidos/mis-pedidos.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Routes, Router } from 'node_modules/@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mis-pedidos',
-  templateUrl: './mis-pedidos.component.html'
+  templateUrl: './mis-pedidos.component.html',
 })
 export class MisPedidosComponent implements OnInit {
-
-  //import { Routes, Router } from 'node_modules/@angular/router';
-  constructor() { }  
-  ngOnInit(): void {
-    
+  public loading = false;
+  public status: string;
+  public pedidos = [];
+  constructor(private readonly httpClient: HttpClient) {}
+  public async ngOnInit(): Promise<void> {
+    this.loading = true;
+    // Cambiar el RFC
+    const RFC = 'BAGN900415TIA';
+    try {
+      this.pedidos = ((await this.httpClient
+        .get(
+          `https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/ventas/webapp/${RFC}`
+        )
+        .toPromise()) as any).body;
+      if (this.pedidos.length) {
+        this.status = 'complete';
+      } else {
+        this.status = 'empty';
+      }
+    } catch (error) {
+      console.log(error);
+      this.status = 'error';
+    } finally {
+      this.loading = false;
+    }
   }
-
 }
