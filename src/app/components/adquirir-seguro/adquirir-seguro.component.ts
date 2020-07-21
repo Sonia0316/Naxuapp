b41935cc0bdf3394222054f12de6c62bb928e1fe
Carlos Eduardo Garcia Tejeda <carlos@numina.mx>
2020-07-21 17:38:20 -0500

    Avances de anticipos

--- src/app/components/adquirir-seguro/adquirir-seguro.component.ts
+++ src/app/components/adquirir-seguro/adquirir-seguro.component.ts
@@ -24,7 +24,7 @@ export class AdquirirSeguroComponent implements OnInit {
   public loading = false;
   public status: string;
   constructor(
-    private httpClient: HttpClient,
+    private readonly httpClient: HttpClient,
     private elRef: ElementRef,
     protected sanitizer: DomSanitizer
   ) {}
--- src/app/components/anticipos/anticipos.component.ts
+++ src/app/components/anticipos/anticipos.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { Routes, Router } from 'node_modules/@angular/router';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-anticipos',
-  templateUrl: './anticipos.component.html'
-  })
-export class AnticiposComponent implements OnInit {
-
-  //import { Routes, Router } from 'node_modules/@angular/router';
-  constructor(public router: Router ) { }
-  ngOnInit(): void {
-    
+  templateUrl: './anticipos.component.html',
+})
+export class AnticiposComponent {
+  public readonly salarioQuincenal = 300;
+  public readonly interesAnticipo = 0.08;
+  public loading = false;
+  public formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  constructor(private readonly httpClient: HttpClient) {}
+  public async requestAdvancePayment() {
+    this.loading = true;
+    try {
+      await this.httpClient
+        .post(
+          'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/emailbackoffice',
+          {
+          }
+        )
+        .toPromise();
+      document.getElementById('showModalExitoSolicitud').click();
+    } catch (error) {
+      console.log(error);
+      document.getElementById('showModalErrorSolicitud').click();
+    } finally {
+      this.loading = false;
+    }
   }
-
 }
