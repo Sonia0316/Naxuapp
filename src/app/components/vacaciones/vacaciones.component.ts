14e6a163c351038884001628abb646f326b10b96
Carlos Eduardo Garcia Tejeda <carlos@numina.mx>
2020-07-26 01:03:09 -0500

    Vacaciones terminadas

--- src/app/components/vacaciones/vacaciones.component.ts
+++ src/app/components/vacaciones/vacaciones.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { Routes, Router } from 'node_modules/@angular/router';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 @Component({
   selector: 'app-vacaciones',
-  templateUrl: './vacaciones.component.html'
+  templateUrl: './vacaciones.component.html',
 })
-export class VacacionesComponent implements OnInit {
+export class VacacionesComponent {
+  public readonly availableDays = 10;
+  public moment = moment;
+  public totalDays = 0;
+  public loading = false;
+  private readonly userRFC = 'BAGN900415TIA';
 
-  //import { Routes, Router } from 'node_modules/@angular/router';
-  constructor() { }
-  ngOnInit(): void {
-    
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public initialChangeDate(initialDate, finalDate) {
+    if (
+      finalDate.value &&
+      !this.moment(initialDate.value).isBefore(finalDate.value)
+    ) {
+      finalDate.value = '';
+    }
   }
 
+  public async calcHolidays(initialDateValue: string, finalDateValue: string) {
+    if (initialDateValue && finalDateValue) {
+      this.loading = true;
+      try {
+        const dataCalc = (await this.httpClient
+          .post(
+            'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/vacaciones/calcula_dias',
+            {
+              start: initialDateValue,
+              end: finalDateValue,
+            }
+          )
+          .toPromise()) as any;
+        this.totalDays =
+          Number(dataCalc.codigo) === 200
+            ? Number(dataCalc.total_dias) || 0
+            : 0;
+      } catch (error) {
+        console.log(error);
+        this.totalDays = 0;
+      } finally {
+        this.loading = false;
+      }
+    } else {
+      this.totalDays = 0;
+    }
+  }
+
+  public async registerHolidays(
+    initialDateValue: string,
+    finalDateValue: string
+  ) {
+    this.loading = true;
+    try {
+      const resultCode = ((await this.httpClient
+        .post(
+          'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/vacaciones',
+          {
+            t12_fechas_inicio: initialDateValue,
+            t12_fecha_fin: finalDateValue,
+            t12_usuario: this.userRFC,
+          }
+        )
+        .toPromise()) as any).codigo;
+      if (Number(resultCode) === 200) {
+        document.getElementById('showModalExitoSolicitud').click();
+      } else {
+        document.getElementById('showModalErrorSolicitud').click();
+      }
+    } catch (error) {
+      console.log(error);
+      document.getElementById('showModalErrorSolicitud').click();
+    } finally {
+      this.loading = false;
+    }
+  }
 }
