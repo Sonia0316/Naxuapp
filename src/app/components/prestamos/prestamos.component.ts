523a7d4c826bb45adba10c37d57ff99f0036d1ae
Carlos Eduardo Garcia Tejeda <carlos@numina.mx>
2020-07-26 03:20:00 -0500

    Avances

--- src/app/components/prestamos/prestamos.component.ts
+++ src/app/components/prestamos/prestamos.component.ts
@@ -23,6 +23,9 @@ export class PrestamosComponent implements OnInit, AfterContentChecked {
 
   public loading = false;
   public status: string;
+  private readonly creditType = 'CREDITO NOMINA';
+  public mainData;
+  public lendData;
 
   public formatter = new Intl.NumberFormat('es-MX', {
     style: 'currency',
@@ -34,25 +37,45 @@ export class PrestamosComponent implements OnInit, AfterContentChecked {
     private changeDetector: ChangeDetectorRef
   ) {}
   public async ngOnInit(): Promise<void> {
-    const years = this.moment().diff(this.startDate, 'years', true);
-    if (years > 1) {
-      switch (true) {
-        case years > 1 && years <= 1.5:
-          this.maxAmountAvailable = this.salarioQuincenal * 2;
-          this.maxPeriods = 3;
-          break;
-        case years > 1.5 && years <= 2:
-          this.maxAmountAvailable = this.salarioQuincenal * 4;
-          this.maxPeriods = 4;
-          break;
-        case years > 2:
-          this.maxAmountAvailable = this.salarioQuincenal * 6;
-          this.maxPeriods = 6;
-          break;
+    this.loading = true;
+    try {
+      this.mainData = ((await this.httpClient
+        .get(
+          'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/creditos/tipocredito'
+        )
+        .toPromise()) as any).response.lista.find(
+        (element) =>
+          element.c05_descripcion === this.creditType &&
+          element.c05_estatus === 'Activo'
+      );
+      if (this.mainData) {
+        const years = this.moment().diff(this.startDate, 'years', true);
+        if (years > 1) {
+          switch (true) {
+            case years > 1 && years <= 1.5:
+              this.maxAmountAvailable = this.salarioQuincenal * 2;
+              this.maxPeriods = 3;
+              break;
+            case years > 1.5 && years <= 2:
+              this.maxAmountAvailable = this.salarioQuincenal * 4;
+              this.maxPeriods = 4;
+              break;
+            case years > 2:
+              this.maxAmountAvailable = this.salarioQuincenal * 6;
+              this.maxPeriods = 6;
+              break;
+          }
+          this.status = 'Available';
+          return;
+        }
       }
-      this.status = 'Available';
-    } else {
       this.status = 'Not available';
+      return;
+    } catch (error) {
+      console.log(error);
+      this.status = 'Not available';
+    } finally {
+      this.loading = false;
     }
   }
   public ngAfterContentChecked(): void {
@@ -62,5 +85,30 @@ export class PrestamosComponent implements OnInit, AfterContentChecked {
   public getPeriodRange(dateRangeValue: number) {
     const value = (dateRangeValue / 100) * this.maxPeriods;
     return `${value} ${value > 1 ? 'Quincenas' : 'Quincena'}`;
+  }
+
+  public async calcLendData(value: number, periods: number) {
+    this.loading = true;
+    try {
+      const data = ((await this.httpClient
+        .post(
+          'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev/creditos/calculadora',
+          {
+            p_prestamo: value,
+            p_id_tipocredito: this.mainData.c05id,
+            p_periodo: periods,
+          }
+        )
+        .toPromise()) as any).response.lista;
+      if (Array.isArray(data) && data.length && data[0].totalpagar !== 'None') {
+        this.lendData = data[0];
+        console.log(this.lendData);
+        document.getElementById('showModalInfoSolicitud').click();
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.loading = false;
+    }
   }
 }
