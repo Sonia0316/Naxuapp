4ccfbc2f150eb2f19be591013a019e81a86d6cbd
Carlos Eduardo Garcia Tejeda <carlos@numina.mx>
2020-09-28 18:34:55 -0500

    Cambios

--- src/app/providers/data.provider.ts
+++ src/app/providers/data.provider.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { DataModel } from '../models/data.interface';
 import asyncLocalStorage from '../util/async-local-storage';
 
 @Injectable()
 export class DataProvider {
   private dataNaxu: DataModel;
-  constructor(private readonly http: HttpClient) {}
-
   public getDataNaxu(): DataModel {
     return this.dataNaxu;
   }
