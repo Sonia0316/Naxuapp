2380fa8edd2b0d2f2e2f2f42afae7653b7262d88
Carlos Eduardo Garcia Tejeda <carlos@hannah.dev>
2021-04-18 14:50:30 -0500

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  mainUrl: 'https://l9ikb48a81.execute-api.us-east-1.amazonaws.com/Dev',
+  mainUrl: 'https://clh7rla63d.execute-api.us-east-1.amazonaws.com/Prod',
 };
--- src/environments/environment2.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  mainUrl: 'https://ltuokc11b2.execute-api.us-east-1.amazonaws.com/Dev',
-};
